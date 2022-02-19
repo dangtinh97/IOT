2f11e1eceb6c6550d4d0a33ec96b1c13ae073b3c
Dang Tinh <>
2022-02-19 21:56:50 +0700

    change port

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ require('dotenv').config()
 import App from "./app";
 import appConfig from './config/app'
 const app = new App();
+console.log('port',appConfig.port)
 app.server.listen(appConfig.port)
