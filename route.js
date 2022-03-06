e79a4c6fbe3fcd10df451078a712611130db74ce
Dang Tinh <>
2022-03-06 09:32:39 +0700

    change url

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const {webhook} = require('./webhook')
-router.get("/webhook",webhook)
+router.get("/webhooks",webhook)
 
 module.exports = router
