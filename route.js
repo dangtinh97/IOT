4d4760ffee8e9ec42b84500175f784bca6e76514
Dang Tinh <>
2023-04-25 00:59:57 +0700

    not file

--- route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {webhook, facebook} = require('./webhook')
-router.get('/facebook',webhook)
-router.post("/facebook",facebook)
-module.exports = router
