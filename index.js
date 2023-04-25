b555bb3a4e17e90600199f30fe41ac10ed2e7bbc
Dang Tinh <>
2023-04-26 00:33:40 +0700

    update socket

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-require('dotenv').config()
-var fs = require('fs');
-var url = require('url');
-var http = require('http');
-var WebSocket = require('ws');
-// function gửi yêu cầu(response) từ phía server hoặc nhận yêu cầu (request) của client gửi lên
-function requestHandler(request, response) {
-    const headers = {
-        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000, // 30 days
-    };
-    response.writeHead(200,headers)
-    response.end('HELLO');
-}
-// create http server
-var server = http.createServer(requestHandler,{
-
-});
-var ws = new WebSocket.Server({
-    server
-});
-var clients = [];
-
-function broadcast(socket, data) {
-    console.log(clients.length);
-    for (var i = 0; i < clients.length; i++) {
-        if (clients[i] !== socket) {
-            clients[i].send(data);
-        }
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const port = process.env.PORT || 3003;
+const io = require("socket.io")(httpServer, {
+    cors:{
+        origins:'*',
+        credentials: true
     }
-}
-ws.on('connection', function(socket, req) {
-    console.log(socket)
-    clients.push(socket);
-    socket.on('message', function(message) {
-        console.log('received: %s', message);
-        broadcast(socket, message);
-    });
-    socket.on('close', function() {
-        var index = clients.indexOf(socket);
-        clients.splice(index, 1);
-        console.log('disconnected');
-    });
 });
-const port = process.env.PORT || 3000
-server.listen(port);
-console.log('Server listening on port '+port);
+
+io.on('connection',(socket)=>{
+    console.log(socket.id)
+    socket.emit('PONG',{})
+})
+httpServer.listen(port,function (){
+    console.log('server start with*:'+port)
+})
