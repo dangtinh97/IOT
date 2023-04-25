ceef9d432f7cb86e3f0ce142cc63a26c1a8c839b
Dang Tinh <>
2023-04-26 01:18:59 +0700

    update socket

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require("express")();
 const httpServer = require("http").createServer(app);
 const port = process.env.PORT || 3003;
 const io = require("socket.io")(httpServer, {
+    path:"/",
     cors:{
         origins:'*'
     }
