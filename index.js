599c90da21b54b8293cc7913498cb7b2b4a99571
Dang Tinh <>
2023-05-01 20:57:57 +0700

    update socket

--- index.js
+++ index.js
@@ -1,37 +1,85 @@
 require('dotenv').config()
-const app = require("express")();
-const httpServer = require("http").createServer(app);
-const port = process.env.PORT || 3003;
-const io = require("socket.io")(httpServer,{
-    cors:{
-        origin:'*'
-    },
-    allowEIO3: true,
-    transports:['websocket']
+const port = process.env.PORT || 3000
+var WebSocket = require('ws');
+var app = require('express')()
+var server = require('http').createServer(app);
+const url = require('url');
+
+const TYPE_ESP8266 = "esp8266";
+const TYPE_CONTROL = "control";
+
+var ws = new WebSocket.Server({
+    server
+});
+let clients = [];
+ws.on('connection', function(socket, req) {
+    let urlObject = url.parse(req.url, true);
+    const isEsp8266 = urlObject.pathname.indexOf('esp8266')!==-1;
+    const isControl = urlObject.pathname.indexOf('control')!==-1;
+    const code = urlObject.query.code;
+    clients.push({
+        socket:socket,
+        code: code,
+        isEsp8266:isEsp8266,
+        isControl:isControl
+    })
+
+    socket.on('message', function(message) {
+        let mess = message.toString();
+        console.log('received: %s', mess);
+        if(mess==="CONTROL_CONNECTED"){
+            let find = findSocket(code,TYPE_ESP8266);
+            if(find!=null){
+                find.send("CONTROL_CONNECTED")
+            }
+        }
+
+        if(mess==="ESP8266_CONNECTED"){
+            let find = findSocket(code,TYPE_CONTROL);
+            if(find!==null){
+                find.send("ESP8266_CONNECTED")
+            }
+        }
+
+        if(mess.indexOf("DEVICE")!==-1){
+            let find = findSocket(code,TYPE_CONTROL);
+            if(find!==null){
+                find.send(mess)
+            }
+        }
+        if(mess.indexOf("EVENT_OUT-")!==-1){
+            let find = findSocket(code,TYPE_ESP8266);
+            if(find!==null){
+                find.send(mess)
+            }
+        }
+    });
+    socket.on('close', function(err) {
+        var index = clients.indexOf(socket);
+        clients.splice(index, 1);
+        console.log('disconnected',err);
+    });
+    ws.on('error', (e)=>{
+        console.log(e)
+    });
 });
 
-io.attach(httpServer,{
-    allowEIO3:true,
-    cookie:true,
-    pingInterval:10000,
-    pingTimeout:5000,
-    transports:['websocket']
-})
+function findSocket(code,type='control')
+{
+    const isControl = type===TYPE_CONTROL;
+    const isEsp8266 = type===TYPE_ESP8266;
+    let filter = clients.filter(item => {
+        return item.isEsp8266 === isEsp8266 && item.isControl === isControl && item.code === code;
+    })
+    if (filter.length === 0) {
+        return null;
+    }
+    return filter[filter.length-1].socket;
+}
 
-
-io.on('connection',(socket)=>{
-    console.log(socket.id)
-    // socket.emit('PONG',{})
-})
-
-io.on('error',(err)=>{
-    console.log(err.stack)
-})
-
-httpServer.listen(port,function (){
-    console.log('server start with*:'+port)
-})
-
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err.stack);
-});
+function time(){
+    let now = new Date()
+    return `${now.getFullYear()}/${now.getMonth()+1}/${now.getDay()}|${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+}
+server.listen(port);
+console.log('Server listening on port:'+port);
